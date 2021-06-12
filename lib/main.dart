fe9db7165700e8010a5abed51a6554712a47b448
Ivan Galarza <igalarza@bnb.com.bo>
2021-06-11 20:15:57 -0400

    basic theme config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/src/theme/theme.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,7 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Music player',
+      theme: myTheme,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Material App Bar'),
